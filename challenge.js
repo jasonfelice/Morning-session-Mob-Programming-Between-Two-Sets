22bc145080da3f2771b275168be5d2281ff58ea6
Jake Felice <jasonfelice0@gmail.com>
2022-06-17 00:33:48 -0700

    Solve

---
+++ challenge.js
@@ -0,0 +1,11 @@
+function getTotalX(a, b) {
+    let count = 0;
+    for(let i = 1; i <= 100; i++) {
+        if(a.every(int => (i % int === 0))) {
+            if(b.every(int => (int % i === 0))) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
