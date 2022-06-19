d90d78d5a28e809baf95608ae262e68e76e9bba8
Jason Felice <jasonfelice0@gmail.com>
2022-06-19 11:49:02 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -3,7 +3,7 @@ function getTotalX(a, b) {
     for(let i = 1; i <= 100; i++) {
         if(a.every(int => (i % int === 0))) {
             if(b.every(int => (int % i === 0))) {
-                count++;
+                count += 1;
             }
         }
     }
